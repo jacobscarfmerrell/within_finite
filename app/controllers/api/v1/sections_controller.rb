885b5cc1b8799a19b477ae993e2bdf58bbfbfb6c
Jacob Merrell <jacobscarfmerrell@gmail.com>
2018-03-28 17:31:12 -0400

    added controllers to simplecov

--- app/controllers/api/v1/sections_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::SectionsController < ApplicationController
-  def index
-    render json: {
-      sections: Section.all,
-      rhythms: Rhythm.all,
-      section_rhythms: SectionRhythm.all
-    }
-  end
-end
--- spec/controllers/rhythms_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RhythmsController, type: :controller do
-
-end
